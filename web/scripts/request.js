8d7263c775e083520c4e9c322dd460bc0b44bd1d
xJesusx0 <jesuspl0411@gmail.com>
2024-08-26 20:53:23 -0500

    funcion para hacer peticiones

---
+++ web/scripts/request.js
@@ -0,0 +1,37 @@
+const request = async (method, url, data = null) => {
+    const token = localStorage.getItem('access_token');
+    //console.log(token)
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    };
+
+    const requestOptions = {
+        url: url,
+        method: method,
+        headers: headers,
+    };
+
+    if (method !== 'GET' && data) {
+        requestOptions.data = data;
+    }
+
+    try {
+        const response = await axios(requestOptions);
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            console.error(error.response.data)
+
+            if(error.response.data.message){
+                alert(error.response.data.message)
+            }
+
+            throw new Error(`Error ${error.response.status}`);
+        } else if (error.request) {
+            throw new Error('No response received from server');
+        } else {
+            throw new Error(`Request failed: ${error.message}`);
+        }
+    }
+};
