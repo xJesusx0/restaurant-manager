ac1d0510c7d03c87914582314a9b5b42869a26d6
xJesusx0 <jesuspl0411@gmail.com>
2024-09-02 16:17:02 -0500

    ruta que devuelve todos los usuarios

--- RestaurantManager/src/main/java/co/jesus/RestaurantManager/rest/AdminController.java
+++ RestaurantManager/src/main/java/co/jesus/RestaurantManager/rest/AdminController.java
@@ -1,4 +1,23 @@
 package co.jesus.RestaurantManager.rest;
 
+import co.jesus.RestaurantManager.database.operations.EmployeesOperations;
+import co.jesus.RestaurantManager.entities.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+
+
+    @GetMapping("/get-employees")
+    public List<Employee> getEmployees (){
+
+        List<Employee> employeeList = EmployeesOperations.getEmployees();
+        return employeeList;
+    }
+
 }
