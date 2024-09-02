02831e41c55ae4ea00e03a6e7fa52777f241822b
xJesusx0 <jesuspl0411@gmail.com>
2024-09-02 17:42:00 -0500

    login funcionando correctamente

--- web/login.js
+++ web/login.js
@@ -1,33 +1,33 @@
-const validateLogin = () => {
-	const username = document.getElementById('username').value;
-	const password = document.getElementById('password').value;
+const validateLogin = async () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-	const url = `${config.SpringApi}/auth/login`;
-	console.log(url);
+    const url = `${config.SpringApi}/auth/login`;
+    console.log('Request URL:', url);
 
-	const requestBody = {
-		'username': username,
-		'password': password
-	}
+    const requestBody = {
+        "username": username,
+        "password": password
+    };
 
 	const roles = {
 		'1':'waiter',
 		'2':'admin',
 		'3':'kitchen'
-	}
+	};
 
-	request('POST', url, requestBody)
-		.then(response => {
-			console.log(response)
-			
-			const role = roles[response.role];
+    try {
+        const response = await request('POST',url,requestBody);
 
-			fetchRoutes()
-			.then(data => {
-				const route = data[role];
-				window.location.href = route;
-			})
-
-		
-		})
-}
+		console.log(response)
+		const role = roles[response.role];
+	
+		fetchRoutes()
+		.then(data => {
+			const route = data[role];
+			window.location.href = route;
+		})		
+    } catch (error) {
+        console.error('Fetch Error:', error);
+    }
+};
