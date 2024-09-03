5383ed8d6b35195d8d93d96c7120b71d27955355
xJesusx0 <jesuspl0411@gmail.com>
2024-09-03 12:07:36 -0500

    operacion que cambia el estado del empleado

--- RestaurantManager/src/main/java/co/jesus/RestaurantManager/database/operations/EmployeesOperations.java
+++ RestaurantManager/src/main/java/co/jesus/RestaurantManager/database/operations/EmployeesOperations.java
@@ -6,20 +6,17 @@ import co.jesus.RestaurantManager.entities.Employee;
 
 import javax.swing.plaf.nimbus.State;
 import javax.xml.transform.Result;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeesOperations {
 
-    public static List<Employee> getEmployees (){
+    public static List<Employee> getEmployees() {
         List<Employee> employees = new ArrayList<>();
 
-        DatabaseOperation GetEmployees = new DatabaseOperation () {
+        DatabaseOperation GetEmployees = new DatabaseOperation() {
 
             @Override
             public void execute(Connection connection) throws SQLException {
@@ -53,4 +50,38 @@ public class EmployeesOperations {
 
     }
 
+    public static int changeStatus(int employeeId) {
+        int rowsAffectedStatus[] = {0};
+        DatabaseOperation ChangeStatus = new DatabaseOperation() {
+            @Override
+            public void execute(Connection connection) throws SQLException {
+                try {
+                    String query = """
+                                UPDATE employees SET status = CASE 
+                                    WHEN status = 'activo' THEN 'inactivo'
+                                    WHEN status = 'inactivo' THEN 'activo'
+                                    ELSE status
+                                END
+                                WHERE employee_id = ?
+                            """;
+                    PreparedStatement statement = connection.prepareStatement(query);
+                    statement.setInt(1, employeeId);
+
+                    int rowsAffected = statement.executeUpdate();
+                    rowsAffectedStatus[0] = rowsAffected;
+                    if (rowsAffected > 0) {
+                        System.out.println("Estado del empleado actualizado correctamente.");
+                    } else {
+                        System.out.println("No se encontró ningún empleado con el ID especificado.");
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            };
+        };
+
+        DatabaseOperationHandler.handreOperation(ChangeStatus);
+
+        return rowsAffectedStatus[0];
+    }
 }
