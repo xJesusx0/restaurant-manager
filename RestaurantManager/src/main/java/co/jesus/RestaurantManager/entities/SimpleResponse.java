627942bf0c7e13d623ea9358ffc000d08c8aa3d9
xJesusx0 <jesuspl0411@gmail.com>
2024-09-03 12:06:19 -0500

    respuesta simple (response:?)

--- RestaurantManager/src/main/java/co/jesus/RestaurantManager/entities/SimpleResponse.java
+++ RestaurantManager/src/main/java/co/jesus/RestaurantManager/entities/SimpleResponse.java
@@ -1,4 +1,17 @@
 package co.jesus.RestaurantManager.entities;
 
 public class SimpleResponse {
+    private String response;
+
+    public SimpleResponse(String response){
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
 }
