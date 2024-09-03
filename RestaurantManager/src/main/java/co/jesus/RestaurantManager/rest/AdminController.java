eb2dab01cf144820450a1d4cb9babcf2d077d072
xJesusx0 <jesuspl0411@gmail.com>
2024-09-03 12:07:10 -0500

    ruta para cambiar el estado de un empleado

--- RestaurantManager/src/main/java/co/jesus/RestaurantManager/rest/AdminController.java
+++ RestaurantManager/src/main/java/co/jesus/RestaurantManager/rest/AdminController.java
@@ -2,9 +2,10 @@ package co.jesus.RestaurantManager.rest;
 
 import co.jesus.RestaurantManager.database.operations.EmployeesOperations;
 import co.jesus.RestaurantManager.entities.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import co.jesus.RestaurantManager.entities.ErrorResponse;
+import co.jesus.RestaurantManager.entities.SimpleResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,9 +16,49 @@ public class AdminController {
 
     @GetMapping("/get-employees")
     public List<Employee> getEmployees (){
-
         List<Employee> employeeList = EmployeesOperations.getEmployees();
         return employeeList;
     }
 
+    @PutMapping("/update-employee-status")
+    public ResponseEntity updateEmployeeStatus(@RequestBody EmployeesModificationBody employeesModificationBody){
+        int id = employeesModificationBody.getEmployeeId();
+        System.out.println(id);
+
+        int rowsAffected = EmployeesOperations.changeStatus(id);
+
+        if(rowsAffected == 0){
+            return ResponseEntity.status(404)
+                    .body(new ErrorResponse("No se modifico ningun usuario"));
+        }
+
+        if(rowsAffected != 1){
+            return ResponseEntity.status(409)
+                    .body(new ErrorResponse("Se han actualizado multiples registros"));
+        }
+
+        return ResponseEntity.status(200).body(new SimpleResponse("Actualizacion exitosa"));
+
+    }
+
 }
+
+class EmployeesModificationBody {
+    private int employeeId;
+
+    public EmployeesModificationBody() {
+    }
+
+    public EmployeesModificationBody(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+}
+
