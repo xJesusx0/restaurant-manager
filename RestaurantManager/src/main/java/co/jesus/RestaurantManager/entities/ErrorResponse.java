6d8b3e909f1f46abfbb33ac53a1f125e5caec5c0
xJesusx0 <jesuspl0411@gmail.com>
2024-09-03 12:06:40 -0500

    respuesta de error

--- RestaurantManager/src/main/java/co/jesus/RestaurantManager/entities/ErrorResponse.java
+++ RestaurantManager/src/main/java/co/jesus/RestaurantManager/entities/ErrorResponse.java
@@ -1,4 +1,18 @@
 package co.jesus.RestaurantManager.entities;
 
 public class ErrorResponse {
+    private String error;
+
+    public ErrorResponse(String error){
+        this.error = error;
+    }
+
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
