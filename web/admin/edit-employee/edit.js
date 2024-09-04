91357dc5971808a44f1d6201cacc57268ed73e85
xJesusx0 <jesuspl0411@gmail.com>
2024-09-04 16:23:08 -0500

    funcionalidad para actualizar los datos de un trabajador

--- web/admin/edit-employee/edit.js
+++ web/admin/edit-employee/edit.js
@@ -7,9 +7,8 @@ const getEmployeeInfo = async () => {
 
     try {
         const response = await request('GET', url);
-        const employee = response; // Asumiendo que `response` es el objeto del empleado
+        const employee = response;
 
-        // Rellenar el formulario con los datos del empleado
         document.getElementById('employeeId').value = employee.employeeId;
         document.getElementById('firstName').value = employee.firstName;
         document.getElementById('lastName').value = employee.lastName;
@@ -25,3 +24,29 @@ const getEmployeeInfo = async () => {
 
 getEmployeeInfo();
 
+const update = async () => {
+    const url = `${config.SpringApi}/admin/update-employee`;
+
+    let employee = {
+        'employeeId': document.getElementById('employeeId').value,
+        'firstName': document.getElementById('firstName').value,
+        'lastName': document.getElementById('lastName').value,
+        'email': document.getElementById('email').value,
+        'phone': document.getElementById('phone').value,
+        'hireDate': document.getElementById('hireDate').value,
+        'position': document.getElementById('position').value,
+        'status': document.getElementById('status').value
+    };
+    
+    console.log(employee);
+
+    try {
+        const response = await request('PUT', url, employee);
+        console.log('Empleado actualizado con éxito:', response);
+        alert('Empleado actualizado correctamente')
+        location.reload();
+
+    } catch (error) {
+        console.error('Error al actualizar el empleado:', error);
+    }
+};
