9f82fa675310fe2c1a9b00cfcb5a48474be93571
xJesusx0 <jesuspl0411@gmail.com>
2024-09-04 16:21:54 -0500

    ruta para obtener los platillos

--- RestaurantManager/src/main/java/co/jesus/RestaurantManager/rest/WaiterController.java
+++ RestaurantManager/src/main/java/co/jesus/RestaurantManager/rest/WaiterController.java
@@ -1,4 +1,25 @@
 package co.jesus.RestaurantManager.rest;
 
+import co.jesus.RestaurantManager.database.operations.DishesOperations;
+import co.jesus.RestaurantManager.entities.Dish;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/waiter")
 public class WaiterController {
+
+    @GetMapping("/get-dishes")
+    public ResponseEntity getDishes(){
+        List<Dish> dishes = DishesOperations.getDishes();
+
+        return ResponseEntity.status(200)
+                .body(dishes);
+
+    }
+
 }
